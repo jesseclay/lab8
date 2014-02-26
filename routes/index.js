854e5c7272a5e9932ddd7fd0f8090e80d2cd7cec
jesseclay <jesseclay@gmail.com>
2014-02-26 13:40:27 -0800

    initial commit

--- routes/index.js
+++ routes/index.js
@@ -7,3 +7,13 @@ var projects = require('../projects.json');
 exports.view = function(req, res){
   	res.render('index', projects);
 };
+
+// exports.view = function(req, res){
+//   var data = {'showAlternate' : false};
+//   res.render('index', data);
+// };
+
+exports.viewAlternate = function(req, res){
+  var data = {'showAlternate' : true};
+  res.render('index', data);
+};
